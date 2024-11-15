a7fdff79f7a1b39bcc6c5a768891407e6988732e
Mohamed Raashid <raashidm862@gmail.com>
2024-11-15 16:20:21 +0530

    add signin functionality to user authentication

--- BackEnd/Controller/userController.js
+++ BackEnd/Controller/userController.js
@@ -6,20 +6,18 @@ const User = require('../Models/UserModel');
 // signup function to register a new user
 const signup = async (req, res) => {
     try {
-        //Extract the name, email, and password from the request body
+
         const { name, email, password } = req.body;
 
         if( !name || !email || !password ){
             return res.status(400).json({message: 'All fields are required'});
         }
 
-        //Check if the user already exists in the database
         const existingUser = await User.findOne({ email });
         if (existingUser){
             return res.status(409).json({message:'User already exists'});
         }
 
-        //Hash the password using bcrypt
         const saltRound = 10;
         const hashedPassword = await bcrypt.hash(password, saltRound);
 
@@ -40,5 +38,39 @@ const signup = async (req, res) => {
 }
 
 // signin function to login a user
+const signin = async (req, res) => {
+    try{
+        const { email, password } = req.body;
 
-module.exports = { signup };
+        if (!email || !password){
+            return res.status(400).json({message: 'Email and Passwod Required'});
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).json({message: 'Invalid Password'});
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+
+        res.status(200).json({
+            message: 'Sign-in successful',
+            token,
+        });
+    }catch(error){
+        res.status(500).json({message: 'Server Error', error: error.message});
+    }
+}
+
+
+
+module.exports = { signup, signin };
